4902795da3088d34fd914fa31013329839f71352
Brais Díaz <braisoncrece@gmail.com>
2023-09-16 14:09:14 +0200

    feat: Dark mode siwtcher (WIP)

--- app/javascript/controllers/toggle_menu_controller.js
+++ app/javascript/controllers/toggle_menu_controller.js
@@ -7,38 +7,10 @@ export default class extends Controller {
         // If the Scroll is on TOP, the nav wont have shadow
         window.addEventListener('scroll', () => {
             const scrollY = window.scrollY;
-            const nav = document.querySelector("nav")
-            const lis = document.querySelectorAll(".nav-link")
-            const mobileMenu = document.querySelector(".mobile-menu")
-            const hamburger = document.querySelector(".hamburguer")
-            const svgPaths = hamburger.querySelectorAll("path");
-
             if (scrollY > 0) {
-                svgPaths.forEach((path) => {
-                    path.setAttribute("stroke", "#FFFFFF")
-                });
                 this.navTarget.classList.add('shadow-lg')
-                nav.classList.remove('bg-white')
-                nav.classList.add('bg-slate-500')
-                mobileMenu.classList.remove("bg-white")
-                mobileMenu.classList.add("bg-slate-500")
-                lis.forEach(li => {
-                    li.classList.remove("text-slate-900")
-                    li.classList.add("text-white")
-                })
             } else {
-                svgPaths.forEach((path) => {
-                    path.setAttribute("stroke", "#03220B")
-                });
                 this.navTarget.classList.remove('shadow-lg')
-                mobileMenu.classList.remove("bg-slate-500")
-                mobileMenu.classList.add("bg-white")
-                nav.classList.add('bg-white')
-                nav.classList.remove('bg-slate-500')
-                lis.forEach(li => {
-                    li.classList.remove("text-white")
-                    li.classList.add("text-slate-900")
-                })
             }
         })
         this.buttonTarget.addEventListener("click", this.toggle.bind(this))
